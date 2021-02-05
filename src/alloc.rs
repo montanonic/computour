74e9c81226b7fb4a8bcffe4c6f9ead46150e1dec
Nicholas Montano <montanonic@gmail.com>
2021-02-05 05:38:39 -0800

    Save

--- src/alloc.rs
+++ src/alloc.rs
@@ -1,18 +1,22 @@
 use std::{
     alloc::{self, GlobalAlloc, Layout, System},
+    cell::{Cell, RefCell},
     error::Error,
-    sync::atomic::{AtomicBool, Ordering},
+    mem, panic,
+    sync::atomic::{AtomicBool, AtomicUsize, Ordering},
 };
 use std::{borrow::BorrowMut, ptr};
 
 pub struct MyAllocator {
     on: AtomicBool,
+    inner: ArrAllocator,
 }
 
 impl MyAllocator {
     pub const fn new() -> Self {
         Self {
             on: AtomicBool::new(false),
+            inner: ArrAllocator::new(),
         }
     }
 
@@ -23,18 +27,105 @@ impl MyAllocator {
     pub fn is_on(&self) -> bool {
         self.on.load(Ordering::SeqCst)
     }
+
+    fn alloc(&self, layout: Layout) -> *mut u8 {
+        self.inner.alloc(layout)
+    }
+
+    fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        self.inner.dealloc(ptr, layout)
+    }
+
+    pub fn print_buf(&self) {
+        unsafe {
+            let msg = "Here's the buffer!\n";
+            libc::write(libc::STDOUT_FILENO, msg.as_ptr() as _, msg.len() as _);
+            libc::write(
+                libc::STDOUT_FILENO,
+                self.inner.arr.as_ptr() as _,
+                self.inner.arr.len() as _,
+            );
+        };
+    }
+
+    /// Allows access to the buffer, disabling custom allocation for the
+    /// duration.
+    pub fn view_buf(&self, f: impl FnOnce((&[u8], &[usize]))) {
+        let last_power = self.is_on();
+        unsafe {
+            self.power(false);
+            f(self.inner.get_buf());
+            self.power(last_power);
+        }
+    }
+}
+
+struct ArrAllocator {
+    /// Points to the next valid spot to write to.
+    write_ptr: AtomicUsize,
+    arr: [u8; 100],
+    layouts: [usize; 100],
+    layout_ptr: AtomicUsize,
+}
+
+impl ArrAllocator {
+    const fn new() -> Self {
+        Self {
+            write_ptr: AtomicUsize::new(0),
+            arr: [0; 100],
+            layouts: [0usize; 100],
+            layout_ptr: AtomicUsize::new(0),
+        }
+    }
+
+    unsafe fn get_buf(&self) -> (&[u8], &[usize]) {
+        (&self.arr, &self.layouts)
+    }
+
+    fn alloc(&self, layout: Layout) -> *mut u8 {
+        let size = layout.size();
+        let align = layout.align();
+        let old_ptr = self.write_ptr.load(Ordering::SeqCst);
+        let mut new_ptr = old_ptr;
+        // First iter will be 0 as old and new ptr are the same. ptr will grow
+        // by align amount, we want it to equal size eventually.
+        while new_ptr - old_ptr < size {
+            new_ptr += align;
+        }
+        self.write_ptr.store(new_ptr, Ordering::SeqCst);
+
+        self.note_layout(layout);
+
+        &self.arr[old_ptr] as *const _ as *mut _
+    }
+
+    fn note_layout(&self, layout: Layout) {
+        let mut ptr = self.layout_ptr.load(Ordering::SeqCst);
+        let arr = &self.layouts as *const _ as *mut [usize; 100];
+        unsafe {
+            (*arr)[ptr] = layout.size();
+            (*arr)[ptr + 1] = layout.align();
+        }
+        self.layout_ptr.store(ptr + 4, Ordering::SeqCst);
+    }
+
+    /// Currently don't actually deallocate.
+    fn dealloc(&self, ptr: *mut u8, layout: Layout) {}
 }
 
 unsafe impl GlobalAlloc for MyAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         if self.is_on() {
-            crate::LAYOUTS.try_lock().map(|mut v| {
-                v.push(layout);
-            });
+            self.alloc(layout)
+        } else {
+            System.alloc(layout)
         }
-        System.alloc(layout)
     }
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        System.dealloc(ptr, layout)
+        if self.is_on() {
+            self.dealloc(ptr, layout)
+        } else {
+            System.dealloc(ptr, layout)
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(alloc_layout_extra)]
 #![warn(rust_2018_idioms)]
 #![allow(unused)]
 
@@ -9,12 +10,22 @@ mod alloc;
 #[global_allocator]
 static ALLOCATOR: alloc::MyAllocator = alloc::MyAllocator::new();
 
-static LAYOUTS: Lazy<Mutex<Vec<Layout>>> = Lazy::new(|| Mutex::new(Vec::with_capacity(1024)));
+// static LAYOUTS: Lazy<Mutex<Vec<Layout>>> = Lazy::new(|| Mutex::new(Vec::with_capacity(1024)));
 
 fn main() {
-    LAYOUTS.lock().unwrap(); // Force initialization.
     ALLOCATOR.power(true);
-    let v = vec![1, 2, 3];
-    ALLOCATOR.power(false);
-    println!("Hello, world! {:?}", LAYOUTS);
+    let v = vec![1u8, 2, 3, 4];
+    let v = vec![5u8, 6, 7, 8, 9, 10, 11, 12];
+    Box::new(u64::MAX);
+    let v = vec![1u8, 2, 3, 4];
+    Box::new(u128::MAX);
+    Box::new(42);
+
+    Box::new(42);
+
+    // let v = vec![100, 200, 300, 400];
+    ALLOCATOR.view_buf(|buf| {
+        println!("{:?}", buf.0);
+        println!("{:?}", buf.1);
+    });
 }
